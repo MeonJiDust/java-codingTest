4405bfffb29936c4f890f8749e915b75cac38d53
MeonJiDust <sikong0685@gmail.com>
2022-01-15 16:23:36 +0900

    01/15 소수만들기 중간 커밋

--- JavaCodingTest/src/level01/PrimeNumberCreat.java
+++ JavaCodingTest/src/level01/PrimeNumberCreat.java
@@ -2,9 +2,38 @@ package level01;
 
 public class PrimeNumberCreat {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public int solution(int[] nums) {
+        int sum = 0;
+        boolean flag = false;
+        
+        for(int i = 0; i < nums.length; i++) {
+        	for(int j = i + 1; j < nums.length; j++) {
+        		for(int k = j + 1; k < nums.length; k++) {
+        			sum = (nums[i] + nums[j] + nums[k]);
+        			
+        			if(sum >= 2) {
+        				flag = primeCheck(sum);
+        			}
+        			if(!flag) {
+        				sum++;
+        			}
+        		}
+        	}
+        }
 
+        return sum;
+    }
+	
+	public boolean primeCheck(int n) {
+		
+		for(int i = 1; i < n; i++) {
+			
+			if(n % 1 == 0) {
+				return true;
+			}
+		}
+		
+		return false;
 	}
-
+	
 }
